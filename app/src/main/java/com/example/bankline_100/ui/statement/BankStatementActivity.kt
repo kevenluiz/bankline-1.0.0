e117a882c5cbae1e1166ea43c3be828def632486
Keven Luiz <kevenluizsojesus@gmail.com>
2025-03-24 12:32:44 -0300

    TEST OK

--- app/src/main/java/com/example/bankline_100/ui/statement/BankStatementActivity.kt
+++ app/src/main/java/com/example/bankline_100/ui/statement/BankStatementActivity.kt
@@ -1,11 +1,15 @@
 package com.example.bankline_100.ui.statement
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.bankline_100.R
+import com.example.bankline_100.databinding.ActivityBankStatementBinding
+import com.example.bankline_100.databinding.ActivityWelcomeBinding
+import com.example.bankline_100.domain.Correntista
 
 class BankStatementActivity : AppCompatActivity() {
 
@@ -13,8 +17,20 @@ class BankStatementActivity : AppCompatActivity() {
         const val EXTRA_ACCOUNT_HOLDER = "com.example.bankline_100.ui.statement.EXTRA_ACCOUNT_HOLDER"
     }
 
+    private val binding by lazy {
+        ActivityBankStatementBinding.inflate(layoutInflater)
+
+
+    }
+
+    private val accountHolder by lazy {
+        intent.getParcelableExtra<Correntista>(EXTRA_ACCOUNT_HOLDER) ?: throw IllegalArgumentException()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bank_statement)
+        setContentView(binding.root)
+
+        Log.d("TESTE", "chegou o id: ${accountHolder.id}")
     }
 }
