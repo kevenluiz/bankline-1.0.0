0c05da8f2207c9217375131ef04cb18f01104f08
Keven Luiz <kevenluizsojesus@gmail.com>
2025-03-25 12:11:59 -0300

    add night.xml,themes.xml, colors.xml and Color.kt; updates in AndroidManifest.xml.

--- app/src/main/java/com/example/bankline_100/ui/Color.kt
+++ app/src/main/java/com/example/bankline_100/ui/Color.kt
@@ -2,10 +2,10 @@ package com.example.bankline_100.ui
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Primary100 = Color(0xFFFFCDD2)
+val Primary200 = Color(0xFFEF9A9A)
+val Primary600 = Color(0xFFE53935)
+val Primary700 = Color(0xFFD32F2F)
+val Primary900 = Color(0xFFB71C1C)
+
--- app/src/main/java/com/example/bankline_100/ui/Theme.kt
+++ app/src/main/java/com/example/bankline_100/ui/Theme.kt
@@ -10,32 +10,23 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
+
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Primary200,
+    secondary = Primary600,
+    tertiary = Primary900
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val LightColorScheme = lightColorScheme(
+    primary = Primary700,
+    secondary = Primary100,
+    tertiary = Primary200
 )
 
 @Composable
 fun Bankline100Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -44,7 +35,6 @@ fun Bankline100Theme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
