7ce066401e286275521613c780120d0575f13374
Keven Luiz <kevenluizsojesus@gmail.com>
2025-03-26 22:42:54 -0300

    update BankStatementActivity.kt

--- app/src/main/java/com/example/bankline_100/ui/statement/BankStatementActivity.kt
+++ app/src/main/java/com/example/bankline_100/ui/statement/BankStatementActivity.kt
@@ -6,10 +6,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bankline_100.R
 import com.example.bankline_100.databinding.ActivityBankStatementBinding
 import com.example.bankline_100.databinding.ActivityWelcomeBinding
 import com.example.bankline_100.domain.Correntista
+import com.example.bankline_100.domain.Movimentacao
+import com.example.bankline_100.domain.TipoMovimentacao
 
 class BankStatementActivity : AppCompatActivity() {
 
@@ -31,6 +34,145 @@ class BankStatementActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        Log.d("TESTE", "chegou o id: ${accountHolder.id}")
+        binding.rvBankStatement.layoutManager = LinearLayoutManager(this)
+
+        findBankStatement()
+    }
+
+    private fun findBankStatement() {
+        val dataSet = ArrayList<Movimentacao>()
+        dataSet.add(Movimentacao(
+            id = 1,
+            dataHora = "26/03/2025 22:09:30",
+            descricao = "Entrada",
+            valor = 1000.0,
+            TipoMovimentacao.RECEITA,
+            idCorrentista = 1)
+        )
+
+        dataSet.add(Movimentacao(
+            id = 1,
+            dataHora = "26/03/2025 22:09:30",
+            descricao = "Entrada",
+            valor = 1000.0,
+            TipoMovimentacao.RECEITA,
+            idCorrentista = 1)
+        )
+
+        dataSet.add(Movimentacao(
+            id = 1,
+            dataHora = "26/03/2025 22:09:30",
+            descricao = "Saida",
+            valor = 1000.0,
+            TipoMovimentacao.DESPESA,
+            idCorrentista = 1)
+        )
+
+        dataSet.add(Movimentacao(
+            id = 1,
+            dataHora = "26/03/2025 22:09:30",
+            descricao = "Entrada",
+            valor = 1000.0,
+            TipoMovimentacao.RECEITA,
+            idCorrentista = 1)
+        )
+        dataSet.add(Movimentacao(
+            id = 1,
+            dataHora = "26/03/2025 22:09:30",
+            descricao = "Saida",
+            valor = 1000.0,
+            TipoMovimentacao.DESPESA,
+            idCorrentista = 1)
+        )
+
+        dataSet.add(Movimentacao(
+            id = 1,
+            dataHora = "26/03/2025 22:09:30",
+            descricao = "Entrada",
+            valor = 1000.0,
+            TipoMovimentacao.RECEITA,
+            idCorrentista = 1)
+        )
+
+        dataSet.add(Movimentacao(
+            id = 1,
+            dataHora = "26/03/2025 22:09:30",
+            descricao = "Saida",
+            valor = 1000.0,
+            TipoMovimentacao.DESPESA,
+            idCorrentista = 1)
+        )
+        dataSet.add(Movimentacao(
+            id = 1,
+            dataHora = "26/03/2025 22:09:30",
+            descricao = "Saida",
+            valor = 1000.0,
+            TipoMovimentacao.DESPESA,
+            idCorrentista = 1)
+        )
+
+        dataSet.add(Movimentacao(
+            id = 1,
+            dataHora = "26/03/2025 22:09:30",
+            descricao = "Saida",
+            valor = 1000.0,
+            TipoMovimentacao.DESPESA,
+            idCorrentista = 1)
+        )
+
+        dataSet.add(Movimentacao(
+            id = 1,
+            dataHora = "26/03/2025 22:09:30",
+            descricao = "Saida",
+            valor = 1000.0,
+            TipoMovimentacao.DESPESA,
+            idCorrentista = 1)
+        )
+
+        dataSet.add(Movimentacao(
+            id = 1,
+            dataHora = "26/03/2025 22:09:30",
+            descricao = "Saida",
+            valor = 1000.0,
+            TipoMovimentacao.DESPESA,
+            idCorrentista = 1)
+        )
+
+        dataSet.add(Movimentacao(
+            id = 1,
+            dataHora = "26/03/2025 22:09:30",
+            descricao = "Saida",
+            valor = 1000.0,
+            TipoMovimentacao.DESPESA,
+            idCorrentista = 1)
+        )
+
+        dataSet.add(Movimentacao(
+            id = 1,
+            dataHora = "26/03/2025 22:09:30",
+            descricao = "Saida",
+            valor = 1000.0,
+            TipoMovimentacao.DESPESA,
+            idCorrentista = 1)
+        )
+
+        dataSet.add(Movimentacao(
+            id = 1,
+            dataHora = "26/03/2025 22:09:30",
+            descricao = "Saida",
+            valor = 1000.0,
+            TipoMovimentacao.DESPESA,
+            idCorrentista = 1)
+        )
+
+        dataSet.add(Movimentacao(
+            id = 1,
+            dataHora = "26/03/2025 22:09:30",
+            descricao = "Saida",
+            valor = 1000.0,
+            TipoMovimentacao.DESPESA,
+            idCorrentista = 1)
+        )
+        binding.rvBankStatement.adapter = BankStatementAdapter(dataSet)
     }
 }
